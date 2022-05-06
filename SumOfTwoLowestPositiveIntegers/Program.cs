63c6547ebad7a4beb29210650f781d3bccdc1631
eniskastrati <eniskastrati18@gmail.com>
2022-05-06 10:12:53 +0200

    Changin the way how it works

--- SumOfTwoLowestPositiveIntegers/Program.cs
+++ SumOfTwoLowestPositiveIntegers/Program.cs
@@ -1,33 +1,43 @@
 ﻿using System;
 
-public static class Program
+namespace sumFoTwoLowestPositiveIntegers
 {
-  
-    public static int Main(int[] numbers)
-    {
-        //sumTwoSmallestNumbers from an array
-        //define variables
-        int result = 0;
-        bool areGood = false;
+     class Program
+    { 
+        public static void Main(string[] args)
+        {
+            double[] numArray = {2, 12,2, 6, 1.3, 5, 120, 123, 11, 11234, 7878};
+            Console.WriteLine(CalcMethod(numArray));
+            Console.ReadLine();
+        }
+
+        public static string CalcMethod(double[] numbers)
+        {
+            //sumTwoSmallestNumbers from an array
+            //define variables
+            string result = " ";
+            bool areGood = false;
 
         
-        //sort the array
-        Array.Sort(numbers);
+            //sort the array
+            Array.Sort(numbers);
         
-        //check if array has floats
-        for(int i = 0; i < numbers.Length; i++){
-            if(numbers[i] % 1 == 0){
-                areGood = true;
-            }else areGood = false;
-        }
+            //check if array has floats
+            for(int i = 0; i < numbers.Length; i++){
+                if(numbers[i] % 1 == 0){
+                    areGood = true;
+                }else areGood = false;
+            }
         
-        //calculate tow last number if all int
-        if(areGood){
-            result = numbers[0] + numbers[1];
-        }else if(areGood == false){
-            result = 0;
+            //calculate tow last number if all int
+            if(areGood){
+                result = Convert.ToString(numbers[0] + " , " + numbers[1]);
+            }else if(areGood == false){
+                result = null;
+            }
+            //return
+            return result;
         }
-        //return
-        return result;
     }
+    
 }
