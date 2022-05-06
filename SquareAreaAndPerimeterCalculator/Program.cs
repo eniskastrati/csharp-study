640e438026aa5f304ed8e57c9db4b7ddd8e8e7cf
eniskastrati <eniskastrati18@gmail.com>
2022-05-06 10:20:17 +0200

    Addin a new sub project to calulate square area and perimeter

---
+++ SquareAreaAndPerimeterCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SquareAreaAndPerimeterCalculator
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int squareheight,area,perimeter;
+            Console.Write("What is the height of your square? :");
+            squareheight = Convert.ToInt32(Console.ReadLine());
+            area = squareheight * squareheight;
+            perimeter = 4 * squareheight;
+            Console.WriteLine("Area : {0}\nPerimeter : {1}",area,perimeter); //   "\n"--> new line
+            Console.ReadKey();
+        }
+    }
+}
