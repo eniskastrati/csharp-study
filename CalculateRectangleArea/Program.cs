8cce5fa4887a8503ee9da1d8c45a9100294f1af5
eniskastrati <eniskastrati18@gmail.com>
2022-05-06 10:17:43 +0200

    Addin a new sub project to calulate rectangel area

---
+++ CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CacluateRectangelArea
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int area, length, width;
+            Console.Write("Please write the length of your rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please write the width of your rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
+            Console.WriteLine("The area of rectangle : {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
