5ae13bc1178ffa61d5087d99b4e554ec2ef964d8
eniskastrati <eniskastrati18@gmail.com>
2022-05-12 18:26:18 +0200

    calculation and output

--- TryCatchThrow_Exercises/Program.cs
+++ TryCatchThrow_Exercises/Program.cs
@@ -13,10 +13,18 @@ namespace TryCatchThrow_Exercises
             Console.WriteLine("###########\n");
             try
             {
+
+                //input
                 Console.Write("Please type in the first number: ");
                 num1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Please type in the first number: ");
                 num2 = Convert.ToInt32(Console.ReadLine());
+
+                //calculation
+                result = num1 / num2;
+
+                //Output
+                Console.WriteLine("{0} / {1} = {2}",num1, num2, result);
             }
             catch (Exception ex)
             {
