7f469b3e10f74fbdcccb58d728ca4712f8fbddc8
eniskastrati <eniskastrati18@gmail.com>
2022-05-16 20:53:24 +0200

    displaying the content in console

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -8,7 +8,18 @@ namespace ReadFile
     {
         static void Main(string[] args)
         {
+            //the path to our file
+            string path = "data.txt";
+
+            //save all text in this array
+            string[] fileContent;
+            fileContent = READ_FILE(path);
             
+            //Print the array to console
+            foreach (var VARIABLE in fileContent)
+            {
+                Console.WriteLine(VARIABLE);
+            }
             Console.ReadKey();
         }
 
@@ -21,7 +32,7 @@ namespace ReadFile
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
                 throw;
             }
         }
