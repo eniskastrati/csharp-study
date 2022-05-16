498b6a9d246f25fb7763c84e9049d3a55a2fa9c5
eniskastrati <eniskastrati18@gmail.com>
2022-05-16 20:39:15 +0200

    addin a method that gets a paramter path and return file content as sting array

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -8,7 +8,14 @@ namespace ReadFile
     {
         static void Main(string[] args)
         {
+            
             Console.ReadKey();
+        }
+
+        public static string [] READ_FILE(sting pathToFile)
+        {
+            string[] fileContent;
+            return fileContent = File.ReadAllLines(pathToFile, Encoding.UTF8);
         }
     }
     
