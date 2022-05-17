c6ea89001f58debde02c538cb0c571b71335f604
eniskastrati <eniskastrati18@gmail.com>
2022-05-17 14:35:40 +0200

    func

--- LocalFileEditor/Program.cs
+++ LocalFileEditor/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace LocalFIleEditor
 {
@@ -9,23 +10,36 @@ namespace LocalFIleEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("#############");
+            string path = "dhcp.txt";
+            string[] dhcpText = CHANGE_FILE(path);
+
+            foreach (var VARIABLE in dhcpText)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            
+            
             Console.ReadKey();
         }
 
-        private static string[] READ_FILE(string pathToFile)
+        public static string[] CHANGE_FILE(string pathToFile)
         {
+            string[] fileContent = null;
             try
             {
-                string[] fileContent;
-                return fileContent = File.ReadAllLines(pathToFile, Encoding.UTF8);
+                fileContent = File.ReadAllLines(pathToFile, Encoding.UTF8);
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    fileContent[i] = fileContent[i].Replace('-', ':');
+                }
+                File.WriteAllLines("dhcp.cfg", fileContent, Encoding.UTF8);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                throw;
             }
-
+            return fileContent;
         }
+        
     }
 }
