06d93cb0b508f3c198e343d9c8bddeacbc550375
eniskastrati <eniskastrati18@gmail.com>
2022-05-17 14:07:39 +0200

    created a new project

---
+++ LocalFileEditor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace LocalFIleEditor
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#############");
+            Console.ReadKey();
+        }
+    }
+}
