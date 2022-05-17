634d973b89c405913256d6843d3a5cb4852f1be9
eniskastrati <eniskastrati18@gmail.com>
2022-05-17 17:46:26 +0200

    Changed File content csc

--- testing/Main.cs
+++ testing/Main.cs
@@ -6,17 +6,26 @@ class Progam
 {
     static void Main(string[] args)
     {
+        string path = "text.txt";
         Console.WriteLine("Enis Kastrati - Linux Mint");
-        string[] allText = File.ReadAllLines("text.txt");
+        string[] allText = File.ReadAllLines(path);
         foreach (string Line in allText)
         {
             Console.WriteLine(Line);
         }
 
-        Console.Write("Please type in a number: ");
-        decimal number = Convert.ToDecimal(Console.ReadLine());
+        string[] newContent = { "Linux", allText.ToString() };
+        File.WriteAllLines(path, newContent);
 
-        Console.WriteLine("You number type is : {0}", number.GetType());
+        foreach (string Line in allText)
+        {
+            Console.WriteLine(Line);
+        }
+
+        // Console.Write("Please type in a number: ");
+        // decimal number = Convert.ToDecimal(Console.ReadLine());
+
+        // Console.WriteLine("You number type is : {0}", number.GetType());
 
         Console.ReadKey();
     }
