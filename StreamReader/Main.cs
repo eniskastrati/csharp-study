b6ec2ba0df9b894be0a3215de59345284abcbf8b
eniskastrati <eniskastrati18@gmail.com>
2022-05-21 00:09:56 +0200

    outputing datat logs formated

--- StreamReader/Main.cs
+++ StreamReader/Main.cs
@@ -7,14 +7,18 @@ class Program
 	static void Main(string[] args)
 	{
 		string path = "logs.csv";
-		string text = null;
+		string[] text = null;
 		try
 		{	
 			StreamReader reader = new StreamReader(path);
 			while(!reader.EndOfStream)
 			{
-				text = reader.ReadLine();
-				Console.WriteLine(text);	
+				text = reader.ReadLine().Split(',');
+				Console.WriteLine("\nLogged In time: \t{0}", text[0]);	
+				Console.WriteLine("Logged In date: \t{0}", text[1]);	
+				Console.WriteLine("Logged Out time: \t{0}", text[2]);	
+				Console.WriteLine("Logged Out date: \t{0}", text[3]);	
+				Console.WriteLine("Username: \t\t{0}\n", text[4]);	
 			}
 			
 		}
