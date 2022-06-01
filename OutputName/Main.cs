b7d1708efc5bc258de29e1a9bd49ae85d9f8ab27
eniskastrati <eniskastrati18@gmail.com>
2022-06-01 10:47:28 +0200

    test project

---
+++ OutputName/Main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Write you name");
+		string name = Console.ReadLine();
+		Console.Write("This is you name" + name);
+		Console.ReadLine();
+	}
+}
